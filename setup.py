f868d5205c27498888778fb8e4faf6c4cc52e499
Chris Beaven <smileychris@gmail.com>
2017-08-08 21:51:54 +1000

    Bump the version number and release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name='uterm',
-    version='0.5',
+    version='0.6',
     author='Chris Beaven',
     author_email='smileychris@gmail.com',
     description='Micropython-friendly terminal',
